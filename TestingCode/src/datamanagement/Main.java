550a07e257acf656de168ff55bf228a4de132614
reban <reban@DESKTOP-3J36UD9.gateway>
2016-09-11 14:52:32 +1000

    code reviewed

--- TestingCode/src/datamanagement/Main.java
+++ TestingCode/src/datamanagement/Main.java
@@ -1,4 +1,10 @@
 package datamanagement;
-
+/** 
+* @author 
+* @version. 
+*/
 public class Main {
-    public static void main(String[] p) {new cgCTL().execute();}    }
+    public static void main(String[] p) {
+        new cgCTL().execute(); }    } /** whitespace error resolved */
+//* @parram p is needed and it should be final. */
+
